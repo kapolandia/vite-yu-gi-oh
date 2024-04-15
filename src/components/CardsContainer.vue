460e70bc27dd1e931f6e0018b20adebc5e77ff4c
kapolandia <paykapo4@gmail.com>
2024-04-15 19:11:44 +0200

    graphic template

---
+++ src/components/CardsContainer.vue
@@ -0,0 +1,12 @@
+<script>
+    export default{
+        name: "CardsContainer"
+    }
+</script>
+
+<template>
+    <div class=" container p-5 bg-white"></div>
+</template>
+
+<style>
+</style>
---
+++ src/components/CardsTemplate.vue
@@ -0,0 +1,12 @@
+<script>
+    export default{
+        name: "CardsTemplate"
+    }
+</script>
+
+<template>
+    <div class=" container p-5 bg-white"></div>
+</template>
+
+<style>
+</style>
