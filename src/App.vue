0eda30d2511741cc94ba0099fdebbd1cdf986332
kapolandia <paykapo4@gmail.com>
2024-04-15 19:48:19 +0200

    push milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
-<script setup>
+<script >
+  import { store } from './store.js';
+  import axios from 'axios';
   import AppHeader from './components/AppHeader.vue'
-  import CardsContainer from './components/CardsContainer.vue'
   import CardsTemplate from './components/CardsTemplate.vue'
 
+  export default {
+    components: {
+      AppHeader,
+      CardsTemplate,
+    },
+    data() {
+      return {
+        store
+      };
+    },
+    methods: {
+      getCardsFromApi() {
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+        .then((response) => {
+          store.cards = response.data;
+          store.isLoading = false;
+          // console.log(store.cards.data[0].card_images[0].image_url);
+          console.log(store.cards.data);
+        });
+      }
+    },
+    mounted() {
+      this.getCardsFromApi();
+    }
+  }
 </script>
 
 <template>
   <AppHeader></AppHeader>
   <main class="p-5">
-    <CardsContainer>
-
-    </CardsContainer>
+      <CardsTemplate :cardsObject="store.cards.data"></CardsTemplate>
   </main>
 </template>
 
--- src/components/CardsTemplate.vue
+++ src/components/CardsTemplate.vue
@@ -1,12 +1,29 @@
 <script>
+
     export default{
-        name: "CardsTemplate"
+        name: "CardsTemplate",
+        props: {
+            cardsObject : Object
+        },
     }
+
 </script>
 
 <template>
-    <div class=" container p-5 bg-white"></div>
-</template>
+    <div class="container p-5 bg-white rounded">
+      <div class="row">
+        <div class="col-12 col-md-6 col-lg-4 col-xl-3 mb-4" v-for="card, index in cardsObject" >
+          <div class="card" style="background-color: #D48F37;min-height: 480px;">
+            <img :src="card.card_images[0].image_url" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title text-white">{{card.name}}</h5>
+              <p class="card-text text-white">{{ card.type }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
 
 <style>
 </style>
